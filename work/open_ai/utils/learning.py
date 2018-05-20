aea781500f37eea4bdec53b0126fab7f5dc1c8bf
tomoki.umezawa <tomoki_umezawa_narumi@yahoo.co.jp>
2018-05-21 01:09:37 +0900

    update

--- work/open_ai/utils/learning.py
+++ work/open_ai/utils/learning.py
@@ -6,12 +6,10 @@ import gym
 import numpy as np
 
 # observationを一つの離散値に変換する
-def digitize_state(env, observation, num_dizitized):
+def digitize_state(observation, low, high, num_dizitized):
     def bins(clip_min, clip_max, num):
         return np.linspace(clip_min, clip_max, num + 1)[1:-1]
-    env_low = env.observation_space.low
-    env_high = env.observation_space.high
     digitized = []
     for index, value in enumerate(observation):
-        digitized.append(np.digitize(value, bins=bins(env_low[index], env_high[index], num_dizitized)))
+        digitized.append(np.digitize(value, bins=bins(low[index], high[index], num_dizitized)))
     return sum([x * (num_dizitized**i) for i, x in enumerate(digitized)])
